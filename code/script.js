e0781a3c89422a901f03f64148b7b93a3b37a635
JuloCS22 <julien.cormont@gmail.com>
2022-07-12 18:06:40 +0200

    Ajout des fonctions

--- code/script.js
+++ code/script.js
@@ -2,12 +2,6 @@ let roll = document.getElementById('roll');
 let hold = document.getElementById('hold');
 let score1 = document.getElementById('score1');
 
-function rollDice() {
-    
-}
-
-
-
 roll.addEventListener('click', () => {
     let dice = Math.floor(Math.random() * (7 - 1)) + 1;
     let result = 0;
