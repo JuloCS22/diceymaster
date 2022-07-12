8a37bf70d12856b25b1603b51f08e4555c86bd95
JuloCS22 <julien.cormont@gmail.com>
2022-07-12 15:58:21 +0200

    ajout dé

---
+++ code/script.js
@@ -0,0 +1,20 @@
+let roll = document.getElementById('roll');
+let hold = document.getElementById('hold');
+let score1 = document.getElementById('score1');
+
+function rollDice() {
+    
+}
+
+
+
+roll.addEventListener('click', () => {
+    let dice = Math.floor(Math.random() * (7 - 1)) + 1;
+    let result = 0;
+    if (dice != 1) {
+        result += dice;
+        score1.innerText = result
+    } else {
+        alert('Dommage, tu as fait 1')
+    }
+    })
