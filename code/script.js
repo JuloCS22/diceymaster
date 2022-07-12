9e4996fa42f470e67b0a1d5088d44a44ac6fd9ed
JuloCS22 <julien.cormont@gmail.com>
2022-07-12 18:06:53 +0200

    Ajout des fonctions

--- code/script.js
+++ code/script.js
@@ -1,14 +1,46 @@
 let roll = document.getElementById('roll');
 let hold = document.getElementById('hold');
 let score1 = document.getElementById('score1');
+let dice = document.getElementById('dice');
+let current1 = document.getElementById('current1')
+let current2 = document.getElementById('current2')
+let total = 0;
+let holdScore = 0;
 
-roll.addEventListener('click', () => {
-    let dice = Math.floor(Math.random() * (7 - 1)) + 1;
-    let result = 0;
-    if (dice != 1) {
-        result += dice;
-        score1.innerText = result
+// Lancer de dé
+
+function diceRoll() {
+    let randomNumber = Math.floor((Math.random() * 6) + 1);
+    return randomNumber;
+}
+
+// Affichage du résultat du lancer de dé
+
+function diceResult(number) {
+    dice.innerText = number
+}
+
+// Fonction Lancer de Dé au click
+
+roll.onclick = function() {
+    let result = diceRoll();
+    if (result != 1) {
+        diceResult(result);
+        total = total + result;
+        current1.innerText = total;
     } else {
-        alert('Dommage, tu as fait 1')
+        alert('Dommage, tu as fait 1');
+        current1.innerText = 0;
+        total = 0;
+        diceResult(0)
     }
-    })
+}
+
+// Fonction Enregistrement du score en cours
+
+hold.onclick = function() {
+    let previousScore = score1.innerText;
+    score1.innerText = Number(total) + Number(previousScore);
+    total = 0;
+    current1.innerText = 0;
+}
