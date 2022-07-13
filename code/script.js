cf06836fe7b6475b22d4ca14bdc02fd3f2c250df
JuloCS22 <julien.cormont@gmail.com>
2022-07-13 08:44:24 +0200

    prompt players names

--- code/script.js
+++ code/script.js
@@ -1,3 +1,5 @@
+let player1 = document.getElementById('player1');
+let player2 = document.getElementById('player2');
 let roll = document.getElementById('roll');
 let hold = document.getElementById('hold');
 let score1 = document.getElementById('score1');
@@ -6,6 +8,13 @@ let current1 = document.getElementById('current1')
 let current2 = document.getElementById('current2')
 let total = 0;
 let holdScore = 0;
+
+// Nom des joueurs
+
+function namePlayers() {
+    player1.innerText = prompt('Quel est le nom du 1er joueur ou de la 1ère joueuse ?')
+    player2.innerText = prompt('Quel est le nom du 2nd joueur ou de la 2nde joueuse ?')
+}
 
 // Lancer de dé
 
@@ -20,7 +29,7 @@ function diceResult(number) {
     dice.innerText = number
 }
 
-// Fonction Lancer de Dé au click
+// Lancer de Dé au click
 
 roll.onclick = function() {
     let result = diceRoll();
@@ -36,7 +45,7 @@ roll.onclick = function() {
     }
 }
 
-// Fonction Enregistrement du score en cours
+// Enregistrement du score en cours
 
 hold.onclick = function() {
     let previousScore = score1.innerText;
